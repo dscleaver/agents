a7cc83af05c6e8ac9cf4417ce2f753e9f947b70e
dscleaver <dcleav001@DIVWL-21TP642A.(none)>
2011-04-14 01:07:32 -0400

    Updating petri interface

--- lib/agents/petri/petri.rb
+++ lib/agents/petri/petri.rb
@@ -7,4 +7,17 @@ class PetriNet
     @total_transitions = 0
   end
 
+  def filled_places 
+    []
+  end
+
+  def active_transitions
+    []
+  end
+
+  def add_place
+    @total_places = @total_places + 1
+    return "P-" + @total_places.to_s
+  end
+
 end
--- spec/petri_spec.rb
+++ spec/petri_spec.rb
@@ -1,13 +1,37 @@
 require 'spec_helper'
 
-describe PetriNet, " after initialization" do
+describe PetriNet do
 
-  it "has no places" do
-    subject.total_places.should == 0
+  describe "after initialization" do
+    it "has no places," do
+      subject.total_places.should == 0
+    end
+
+    it "no transitions," do
+      subject.total_transitions.should == 0
+    end
+
+    it "no places with tokens," do
+      subject.filled_places.should be_empty
+    end
+
+    it "and no active transitions" do
+    end
   end
 
-  it "and no transitions" do
-    subject.total_transitions.should == 0
+  describe "when adding a Place" do
+
+    before(:all) do
+      @id = subject.add_place
+    end
+
+    it "should have one place" do
+      subject.total_places.should == 1
+    end
+
+    it "return an assigned id" do
+      "P-1".should === @id
+    end
   end
 
 end
